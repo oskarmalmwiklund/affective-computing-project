3db467cadab2883d37d5688d618224544459a181
kateboni <kateboni@outlook.com>
2023-06-29 20:55:28 +0200

    Summary and video page

--- Affective Computing Web/popup.js
+++ Affective Computing Web/popup.js
@@ -5,3 +5,11 @@ function openPopup() {
 function closePopup() {
     document.getElementById("popup").style.display = "none";
 }
+
+function openPopup2() {
+    document.getElementById("popup2").style.display = "block";
+}
+
+function closePopup2() {
+    document.getElementById("popup2").style.display = "none";
+}
